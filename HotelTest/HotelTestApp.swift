bca9e3ee4fe1f83c5538eece1d83ac59a81da23d
gadjishka <gereygadji@gmail.com>
2023-08-30 17:58:45 +0300

    Initial Commit

---
+++ HotelTest/HotelTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelTestApp.swift
+//  HotelTest
+//
+//  Created by Гаджи Герейакаев on 30.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
